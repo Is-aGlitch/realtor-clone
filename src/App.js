67d3bd777b128c37a9bfb0ed1a9abf9fc53b4762
Ankita Singh <ankitakumari.ece1@bvp.edu.in>
2023-10-08 16:18:35 +0530

    Create Pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./Pages/Home"
+import Profile from "./Pages/Profile"
+import Offers from "./Pages/Offers"
+import SignIn from "./Pages/SignIn"
+import SignUp from "./Pages/SignUp"
+import ForgotPassword from "./Pages/ForgotPassword"
 
 function App() {
   return (
     <>
-      <h1>Hellow World!!!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element = {<Home />} />
+          <Route path="/profile" element = {<Profile />} />
+          <Route path="/offers" element = {<Offers />} />
+          <Route path="/sign-in" element = {<SignIn />} />
+          <Route path="/forgot-password" element = {<ForgotPassword />} />
+          <Route path="/sign-up" element = {<SignUp />} />
+        </Routes>
+      </Router>
     </>
   );
 }
